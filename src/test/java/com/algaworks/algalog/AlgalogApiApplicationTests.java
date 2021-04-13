e070c809493042aa48126221355c2746e1c14f2b
Thiago Faria <thiagofa@gmail.com>
2021-04-12 23:07:01 -0300

    Adicionado teste fake

--- src/test/java/com/algaworks/algalog/AlgalogApiApplicationTests.java
+++ src/test/java/com/algaworks/algalog/AlgalogApiApplicationTests.java
@@ -1,5 +1,7 @@
 package com.algaworks.algalog;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +11,10 @@ class AlgalogApiApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	
+	@Test
+	void fakeTest() {
+		assertTrue(false);
+	}
+	
 }
